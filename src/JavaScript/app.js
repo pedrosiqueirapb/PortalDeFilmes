698144e21a83aef24d30d5695b168dff4aa3c19f
pedrosiqueirapb <pedrosiqueirapb@gmail.com>
2023-06-25 17:00:50 -0300

    Requisição com fetch

--- src/JavaScript/app.js
+++ src/JavaScript/app.js
@@ -1,16 +1,30 @@
+/*
 // instanciando objeto para fazer requisição
 var xhr = new XMLHttpRequest();
 
 // quando o estado do objeto mudar
 xhr.onreadystatechange = ()=>{
     if(xhr.readyState == 4 && xhr.status == 200)
-    // estado (4) = concluído
-    // status (200) = ok
+        // estado (4) = concluído
+        // status (200) = ok
         console.log(xhr.response); // escreve no console o retorno da requisição
 }
 
 // tipo da requisição e a url do servidor
-xhr.open("GET", "https://api.themoviedb.org/3/movie/550?api_key=32f42fc190830e705f73dca17d56ebf7");
+xhr.open("GET", "https://api.themoviedb.org/3/movie/popular?api_key=32f42fc190830e705f73dca17d56ebf7");
 
 // mandando requisição
 xhr.send();
+*/
+
+fetch("https://api.themoviedb.org/3/movie/popular?api_key=32f42fc190830e705f73dca17d56ebf7")
+    .then(response=>{
+        return response.json();
+    })
+    .then(json =>{
+        console.log(json);
+        let i;
+        for(i=0; i<10; i++){
+            document.querySelector('main').innerHTML += `${json.results[i].original_title}<br>`;
+        }
+    })
